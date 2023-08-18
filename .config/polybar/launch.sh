029e5375f82e12427090feb72f5e0a40f4bb3960
Nezmet <nezmet@proton.me>
2023-08-17 22:50:22 -0400

    added dual monitor configs for polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,14 +1,32 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
 polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch nez
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar nez 2>&1 | tee -a /tmp/polybar1.log & disown
-#polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+# MIGHT GET THIS TO WORK LATER......
 
-echo "Bars launched..."
+# Wait for processes
+#while pgrep -x polybar >/dev/null; do sleep 1; done
+#
+#screens=$(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f6)
+#
+#if [[ $(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f4 | cut -d"+" -f2- | uniq | wc -1) == 1 ]]; then
+#  MONITOR=$(polybar --list-monitors | cut-d":" -f1) TRAY_POS=right polybar main &
+#else
+#  primary=$(xrandr --query | grep primary | cut -d" " -f1)
+#
+#  for m in $screens; do
+#    if [[ $primary == $m ]]; then
+#      MONITOR=$m TRAY_POS=right polybar main $
+#    else
+#      MONITOR=$m TRAY_POS=none polybar secondary &
+#    fi
+#  done
+#fi 
+
+# Launch bars
+polybar main & disown
+polybar secondary & disown
+
+#echo "Bars launched..."
